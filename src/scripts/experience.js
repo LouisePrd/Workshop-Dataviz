c6be4133dc4d8e17f9dd1627c5f42b08592f7715
victorcngo <hello@victorcango.fr>
2022-12-06 11:27:20 +0100

    fix: init Paperjs properly

--- src/scripts/experience.js
+++ src/scripts/experience.js
@@ -2,21 +2,43 @@
 
 // const app = new GameApp(document.body, window.innerWidth, window.innerHeight)
 
-import Paper from 'paper'
+import paper, { view } from 'paper'
+// import { Noise } from 'noisejs'
 
-function initPaper() {
-  console.log(Paper)
+// let noise_seed = Math.random()
+// const NOISE_GENERATOR = new Noise(noise_seed)
+const CANVAS = document.getElementById('visualizer')
+const NUM_CURVES = 30
+const PATH_SEGMENT_LENGTH = 20
+const X_SPACING = CANVAS.clientWidth / NUM_CURVES
+const Y_SPACING = CANVAS.clientHeight / PATH_SEGMENT_LENGTH
 
-  let path = new Paper.Path()
-  path.strokeColor = 'black'
-  path.add(new Point(30, 30))
-  path.add(new Point(100, 100))
-
-  const paperCanvas = document.getElementById('#visualizer')
-  const project = new Paper.Project(paperCanvas)
+window.onload = function () {
+  paper.setup(CANVAS)
+  paper.install(window)
+  view.onFrame = animate
+  init()
 }
 
-window.addEventListener('load', () => {
-  initPaper()
-  // Do stuff
-})
+const init = () => {
+  render_curves()
+}
+
+const render_curves = () => {
+  for (let i = 0; i <= NUM_CURVES; i++) {
+    // const myPath = new Path()
+    // myPath.selected = true
+    // myPath.strokeColor = '#00c0c0'
+    // myPath.add(new Point(i * X_SPACING, 0))
+
+    const path = new Path()
+    path.strokeColor = 'black'
+    path.add(new Point(30, 30))
+    path.add(new Point(100, 100))
+  }
+}
+
+function animate(event) {
+  // seed = seed + 0.0025
+  // 0.3 * noise.simplex2(noise_seed + ix / 40, noise_seed + iy / 180)
+}
